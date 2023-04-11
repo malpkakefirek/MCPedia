68ef6f83d8d43e0776bbebe3973d11541e0dadf7
malpkakefirek <5319691-malpkakefirek@users.noreply.replit.com>
2023-04-11 14:02:42 +0000

    Added embed as a response

--- cogs/wiki.py
+++ cogs/wiki.py
@@ -24,12 +24,25 @@ class Wiki(discord.Cog):
         ),
     ):
         await ctx.defer()
+        wiki_url = "https://minecraft.fandom.com/wiki/"
         page_id = fandom.search(search, results=1)[0][1]
         page = fandom.page(pageid=page_id)
-        html = Soup(page.html, 'html.parser')
-        print([img['src'] for img in html.find_all('img')])
-        images_links = "\n**Links**"
-        await ctx.respond(images_links)
+        # html = Soup(page.html, 'html.parser')
+        embed = discord.Embed(
+            title = page.title,
+            url = wiki_url + page.title.replace(" ", "_"),
+            description = page.summary,
+            color = discord.Color(43520),    # 00AA00 (dark green)
+        )
+        # view = discord.ui.View()
+        # section_select = discord.ui.Select() # TODO: ~~create custom select class~~ NEVERMIND! There are too many sections to fit into a select menu! Convert to command option + pagination arrows?
+        # for section in page.sections:
+            # try:
+                # section_select.add_option(label=section)
+            # except:
+                # break
+        # view.add_item(section_select)
+        await ctx.respond(embed=embed)
 
 def setup(bot):
     bot.add_cog(Wiki(bot))
