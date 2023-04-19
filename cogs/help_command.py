b3ff39f83ea3f6f77613c2a9bdbd43aa509653b4
malpkakefirek <72975781+malpkakefirek@users.noreply.github.com>
2023-04-19 19:04:28 +0000

    Updated help for slash commands + bumped version

--- cogs/help_command.py
+++ cogs/help_command.py
@@ -1,6 +1,5 @@
 import discord
 from discord.errors import Forbidden
-from replit import db
 
 """
 « HELP COG »
@@ -10,8 +9,9 @@ https://gist.github.com/StudioMFTechnologies/ad41bfd32b2379ccffe90b0e34128b8b
 Rewritten and optimized by github.com/nonchris
 https://gist.github.com/nonchris/1c7060a14a9d94e7929aa2ef14c41bc2
 
-Rewritten again and modified for my own purposes by github.com/malpkakefirek | Malpkakefirek#0001
+Rewritten again and heavily modified for my own purposes by github.com/malpkakefirek | Malpkakefirek#0001
 """
+
 
 async def send_embed(ctx, embed):
     """
@@ -21,41 +21,55 @@ async def send_embed(ctx, embed):
         await ctx.respond(embed=embed)
     except Forbidden:
         try:
-            await ctx.send("I can't send embeds. Check my pemissions :)")
+            await ctx.send("I can't send embeds. Check my pemissions")
         except Forbidden:
+            response = "Hey, it looks like I can't send messages in channel %s in %s\nCould you notify the owner about this? :slight_smile: "
             await ctx.author.send(
-                f"Hey, it looks like I can't send embeds in channel {ctx.channel.name} in {ctx.guild.name}\n"
-                "Could you notify the owner about this? :slight_smile: ",
+                response % (ctx.channel.name, ctx.guild.name),
                 embed=embed
             )
 
 
 class Help(discord.Cog):
     """Help cog"""
-    
+
     def __init__(self, bot):
         self.bot = bot
         print(f"** SUCCESSFULLY LOADED {__name__} **")
 
-    @discord.slash_command(name="help")
-    async def help_slash(self, ctx, *, input: discord.commands.Option(str, "A command or category, you want more info on", default=None)):
-        """Helpful if you forget how to use commands"""
-
+    @discord.slash_command(
+        name="help",
+        description="Helpful if you forget how to use commands",
+    )
+    async def help_slash(
+        self,
+        ctx,
+        command_or_category: discord.commands.Option(
+            str,
+            description = "A command or category, you want more info on",
+            default = None
+        )
+    ):
         prefix = "/"
         secondary_prefix = "m!"
-        version = "0.1"
-        owner_ids = db['permitted']
+        version = "1.3"
+
+        # setting owner name
+        owner_name = "MCPedia Team"
+        #avatar = self.bot.get_user(owner_id).avatar.ur
 
         # checks if cog parameter was given
         # if not: sending all modules and commands not associated with a cog
-        if not input:
+        if not command_or_category:
             # checks if owner is on this server - used to 'tag' owner
+            owner = owner_name
+            owner_ids = self.bot.owner_ids
 
             # starting to build embed
             emb = discord.Embed(
-                title=':sparkles: __All commands__ :sparkles:',
-                color=discord.Color.teal(),
-                description=f'Use `{prefix}help <command/category>` to get additional info about a command/category'
+                title=":sparkles: __All commands__ :sparkles:",
+                color=discord.Color.blue(),
+                description="Use `/help <command/category>` to get additional info about a command/category",
             )
 
             # iterating through cogs, gathering descriptions
@@ -67,82 +81,94 @@ class Help(discord.Cog):
                 for command in temp_cog.get_commands():
                     # if command in a cog
                     # listing command if cog name is None and command isn't hidden
-                    try:
+                    if isinstance(command, discord.SlashCommandGroup):
                         for subcommand in command.subcommands:
-                            commands_desc += f'`{prefix}{subcommand.qualified_name or subcommand.name}` - {subcommand.description if subcommand.description else "No description provided"}\n'
-                    except:
-                        try:
-                            if not command.hidden or int(ctx.author.id) in owner_ids:
-                                commands_desc += f'`{secondary_prefix}{command.name}` - {command.description or "No description provided"}\n'
-                        except:
-                            commands_desc += f'`{prefix}{command.name}` - {command.description or "No description provided"}\n'
+                            mention = f"</{command.name} {subcommand.name}:{command.id}>"
+                            description = subcommand.description or "No description"
+                            commands_desc += f'{mention} - {description}\n'
+                    elif isinstance(command, discord.SlashCommand):
+                        description = command.description or "No description" 
+                        commands_desc += f'{command.mention} - {description}\n'
+                    # text command
+                    elif isinstance(command, discord.ext.commands.Command):
+                        if not command.hidden or int(ctx.author.id) in owner_ids:
+                            description = command.brief or "No description"
+                            commands_desc += f'`{secondary_prefix}{command.name}` - {description}\n'
+                    else:
+                        commands_desc += "???"
+                        print(command.name)
 
+                # adding those commands to embed
                 if commands_desc:
                     emb.add_field(
-                        name=f"Category {cog.title()}",
+                        name=f"Category {cog}",
                         value=commands_desc,
-                        inline=False
+                        inline=False,
                     )
 
             commands_desc = ''
             for command in self.bot.walk_application_commands():
-                # if command not in a cog
-                # listing command if cog is None
+                # if slash command not in a cog
                 if not command.cog:
-                    commands_desc += f'`{prefix}{command.qualified_name}` - {command.description or "No description provided"}\n'
+                    description = command.description or "No description"
+                    commands_desc += f'{command.mention} - {description}\n'
             for command in self.bot.walk_commands():
-                # if command not in a cog
-                # listing command if cog is None and command isn't hidden
+                # if text command not in a cog and not hidden
                 if (not command.hidden or int(ctx.author.id) in owner_ids) and not command.cog:
-                    commands_desc += f'`{secondary_prefix}{command.name}` - {command.description or "No description provided"}\n'
+                    description = command.brief or "No description"
+                    commands_desc += f'`{secondary_prefix}{command.name}` - {description}\n'
 
             if commands_desc:
                 emb.add_field(
                     name="No category",
                     value=commands_desc,
-                    inline=False
+                    inline=False,
                 )
 
             # setting information about author
             emb.set_footer(
-                text=f"Version {version}"
+                text=f"Made by {owner} | Version {version}",
+                #icon_url=avatar,
             )
 
-            # block called when one cog-name is given
-            # trying to find matching cog and it's commands
+        # block called when one cog-name or command is given
+        # trying to find matching cog and it's commands
         else:
             used_prefix = prefix
             found_command = None
             # iterating through cogs
             for cog in self.bot.cogs:
                 # check commands first
-                if cog.lower() != input.lower():
+                if cog.lower() != command_or_category.lower():
                     for command in self.bot.get_cog(cog).get_commands():
                         try:
                             for subcommand in command.subcommands:
-                                if subcommand.qualified_name.lower() == input.lower():
+                                if subcommand.qualified_name.lower() == command_or_category.lower():
                                     found_command = subcommand
                                     break
-                        except:
-                            if command.qualified_name.lower() == input.lower():
-                                try:
-                                    if command.hidden and int(ctx.author.id) not in owner_ids:
-                                        continue
-                                except:
+                        except Exception:
+                            try:
+                                if command.hidden and int(ctx.author.id) not in owner_ids:
+                                    continue
+                                if command.name.lower() == command_or_category.lower():
+                                    used_prefix = secondary_prefix
                                     found_command = command
-                        
+                            except Exception:
+                                if command.qualified_name.lower() == command_or_category.lower():
+                                    found_command = command
+
                         if found_command:
                             break
                     if found_command:
                         break
-                
+
                 # if it's a cog
                 else:
                     # making title - getting description from doc-string below class
                     emb = discord.Embed(
-                        title=f'{cog} - Commands',
+                        title=f"{cog} - Commands",
                         description=self.bot.cogs[cog].__doc__,
-                        color=discord.Color.teal()
+                        color=discord.Color.blue()
                     )
 
                     # getting commands from cog
@@ -150,21 +176,26 @@ class Help(discord.Cog):
                         try:
                             for subcommand in command.subcommands:
                                 emb.add_field(
-                                    name=f"{prefix}{subcommand.qualified_name}",
-                                    value=subcommand.description or "No description provided",
-                                    inline=False
+                                    name=f"</{subcommand.qualified_name}:{command.id}>",
+                                    value=subcommand.description or "No description",                                    
+                                    inline=False,
                                 )
-                        except:
+                        except Exception:
                             try:
                                 if command.hidden and int(ctx.author.id) not in owner_ids:
                                     continue
-                            except:
-                                pass
-                            emb.add_field(
-                                name=f"{prefix}{command.name}",
-                                value=command.description or "No description provided",
-                                inline=False
-                            )
+                            except Exception:
+                                emb.add_field(
+                                    name=f"{command.mention}",
+                                    value=command.description or "No description",
+                                    inline=False,
+                                )
+                            else:
+                                emb.add_field(
+                                    name=f"{secondary_prefix}{command.name}",
+                                    value=command.brief or "No description",
+                                    inline=False,
+                                )
                     # found cog - breaking loop
                     break
             # if command not found in a cog
@@ -174,44 +205,45 @@ class Help(discord.Cog):
                     try:
                         if command.cog or (command.hidden and int(ctx.author.id) not in owner_ids):
                             continue
-                    except:
-                        if not command.cog and command.qualified_name.lower() == input.lower():
+                    except Exception:
+                        if not command.cog and command.qualified_name.lower() == command_or_category.lower():
                             found_command = command
                             break
                 # if it's not a walk slash command
-                else:    
+                else:
                     # check all walk message commands
                     for command in self.bot.walk_commands():
                         try:
                             if command.cog or (command.hidden and int(ctx.author.id) not in owner_ids):
                                 continue
-                        except:
+                        except Exception:
                             pass
-                        
-                        if not command.cog and command.name.lower() == input.lower():
+
+                        if not command.cog and command.name.lower() == command_or_category.lower():
                             found_command = command
                             used_prefix = secondary_prefix
                             break
                     else:
                         emb = discord.Embed(
                             title="What's that?!",
-                            description=f"I've never heard of `{input}` :scream:",
-                            color=discord.Color.orange()
+                            description=f"I've never heard of `{command_or_category}` :scream:",
+                            color=discord.Color.orange(),
                         )
-            
+
             if found_command:
+                command_name = found_command.qualified_name or found_command.name
                 emb = discord.Embed(
-                    title=f':sparkles: __{used_prefix}{found_command.qualified_name or found_command.name}__ :sparkles:',
-                    color=discord.Color.teal()
+                    title=f':sparkles: __{used_prefix}{command_name}__ :sparkles:',
+                    color=discord.Color.blue(),
                 )
                 emb.add_field(
                     name="How does it work?",
-                    value=found_command.description or "No description provided",
-                    inline=False
+                    value=found_command.description or "No description",
+                    inline=False,
                 )
-                
-        await send_embed(ctx, emb)
 
+        # sending reply embed using our own function defined above
+        await send_embed(ctx, emb)
 
 
 def setup(bot):
