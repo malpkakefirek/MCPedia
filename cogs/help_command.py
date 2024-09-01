b72effbf49ab96235c7b69ce45508fb890e75846
malpkakefirek <malpkakefirek@gmail.com>
2024-09-01 20:00:52 +0200

    bump version

--- cogs/help_command.py
+++ cogs/help_command.py
@@ -52,7 +52,7 @@ class Help(discord.Cog):
     ):
         prefix = "/"
         secondary_prefix = "m!"
-        version = "2.3"
+        version = "2.4"
 
         # setting owner name
         owner_name = "MCPedia Team"
